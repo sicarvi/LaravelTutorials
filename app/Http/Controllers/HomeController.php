3ad57139350c2eeb0260b1e1c2edd520617ec173
Simon <scardenasv@eafit.edu.co>
2023-05-10 06:41:30 -0500

    lint in github

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,13 @@ use Illuminate\View\View;
 class HomeController extends Controller
 {
     public function index(): View
+
+
     {
-        return view('home.index');
-    }
+                
+                    return view('home.index');}
+
+
+
+                    
 }
